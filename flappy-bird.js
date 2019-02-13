7961f2754084d11209d778c4e4129dade6e6b576
cameronwatson00 <cameronwatson00@gmail.com>
2019-02-13 14:58:48 +1100

    Pipes Done

--- flappy-bird.js
+++ flappy-bird.js
@@ -1,24 +1,20 @@
 let bird;
 let pipes;
-let count;
 
 function setup(){
 	createCanvas(550,650);
 	bird = new Bird();
 	pipes = [];
-	count = 0;
+	setInterval(newPipe, 2000);
+}
+
+function newPipe(){
+	pipes.push(new Pipe());
 }
 
 function draw(){
 	background(51);
-	count++;
-
-	if(count == 150){
-		pipes.push(new Pipe());
-		count = 0;
-	}
-
-
+	
 	for(let i = 0; i < pipes.length; i++){
 		pipes[i].show();
 		pipes[i].update();
--- pipe.js
+++ pipe.js
@@ -1,24 +1,22 @@
 function Pipe(){
 
 	this.xpos = width+1;
-	this.distanceBetween = 150;
+	this.distanceBetween = 120;
 	this.w = 55;
+	this.color = 'green';
 
-	this.topBar = random(50,height-150);
+	this.topBar = random(50,height-200);
 	this.bottomBar = this.topBar + this.distanceBetween;
 
 	this.show = function(){
 		stroke(0);
 		strokeWeight(2);
-		fill(0,255,0);
+		fill(this.color);
 		rect(this.xpos, -1, this.w ,this.topBar);
-		rect(this.xpos, this.bottomBar, this.w ,height - this.bottomBar + 1);
+		rect(this.xpos, this.bottomBar, this.w ,height - this.topBar + 1);
 	}
 
 	this.update = function(){
 		this.xpos -= 3;
 	}
-	
-
-	// fine tune numbers to ensure its all even
 }
